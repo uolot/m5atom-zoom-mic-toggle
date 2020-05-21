8e4c0147d2a6a21e6ce1a8a10756a146510da566
Tomasz Walotek <tomasz.walotek@gmail.com>
2020-05-21 16:02:26 +0100

    Add support for Google Hangouts

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Microphone toggle button for Zoom client using M5Atom and BLE
+# Microphone toggle button for Zoom and Hangouts clients using M5Atom and BLE
 
 - Runs on [M5Atom Matrix](https://m5stack.com/collections/m5-atom/products/atom-matrix-esp32-development-kit)
 - Acts as a BLE keyboard using [ESP32-BLE-Keyboard by T-vK](https://github.com/T-vK/ESP32-BLE-Keyboard)
 - Sends `<Alt+a>` to toggle mic during a call in Zoom client
+- Sends `<Ctrl-d>` to toggle mic in Hangouts client
+- Keep button pressed on boot to enable Hangouts mode, default is Zoom
 
 Demo:
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,15 @@
 
 BleKeyboard bleKeyboard;
 
+#define ZOOM 1
+#define HANGOUTS 2
+#define CZOOM 0x0000ff
+// FIXME: why 0x00ff00 gives red? is it GRB?
+#define CHANGOUTS 0xff0000
+
+int mode;
+int color;
+
 
 void fill(int color)
 {
@@ -30,25 +39,44 @@ void setup()
 {
     M5.begin(true, false, true);
     M5.dis.setBrightness(BRIGHTNESS);
+
+    if (M5.Btn.isPressed()) {
+        mode = HANGOUTS;
+        color = CHANGOUTS;
+    }
+    else {
+        mode = ZOOM;
+        color = CZOOM;
+    }
+
     bleKeyboard.begin();
     waitForConnection();
-    fill(CRGB::Blue);
+
+    fill(color);
     delay(500);
 }
 
 void toggleMic()
 {
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.write('a');
-    delay(10);
-    bleKeyboard.release(KEY_LEFT_ALT);
+    if (mode == HANGOUTS) {
+        bleKeyboard.press(KEY_LEFT_CTRL);
+        bleKeyboard.write('d');
+        delay(10);
+        bleKeyboard.release(KEY_LEFT_CTRL);
+    }
+    else {
+        bleKeyboard.press(KEY_LEFT_ALT);
+        bleKeyboard.write('a');
+        delay(10);
+        bleKeyboard.release(KEY_LEFT_ALT);
+    }
 }
 
 void loop()
 {
     if (M5.Btn.wasPressed()) {
         toggleMic();
-        fill(CRGB::Blue);
+        fill(color);
 
         for (int b = BRIGHTNESS; b > 0; b--) {
             M5.dis.setBrightness(b);
